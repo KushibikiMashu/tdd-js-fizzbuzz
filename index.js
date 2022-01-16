b45a47a9741707ca38193e9ee3d4ad4467de8d95
KushibikiMashu <kushibiki3413@gmail.com>
2022-01-16 17:41:32 +0900

    feat: FizzBuzzを実装する

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function add(a, b) {
-  return a + b;
+function fizzBuzz(num) {
+  const canDivideByThree = num % 3 === 0;
+  const canDivideByFive = num % 5 === 0;
+
+  if (canDivideByThree && canDivideByFive) {
+    return 'FizzBuzz';
+  } else if (canDivideByThree) {
+    return 'Fizz';
+  } else if (canDivideByFive) {
+    return 'Buzz';
+  }
+  return num.toString();
 }
 
-module.exports = add
+module.exports = fizzBuzz;
