5e3dab1556a026a5513101197f46b100df1e43f2
KushibikiMashu <kushibiki3413@gmail.com>
2022-01-21 22:37:45 +0900

    test: テストの表記を変更

--- index.test.js
+++ index.test.js
@@ -21,7 +21,7 @@ describe('canDivideBy', () => {
 });
 
 describe('fizzBuzz', () => {
-  describe('数を文字列に変換する', () => {
+  describe('数値を文字列に変換する', () => {
     test("1を渡すと文字列'1'を返す", () => {
       expect(fizzBuzz(1)).toBe('1');
     });
@@ -32,13 +32,13 @@ describe('fizzBuzz', () => {
   });
 
   describe('3の倍数を渡すと「Fizz」を返す', () => {
-    test("3を渡すと文字列'Fizz'を返す", () => {
+    test("3を渡すと文字列「Fizz」を返す", () => {
       expect(fizzBuzz(3)).toBe('Fizz');
     });
   });
 
   describe('5の倍数を渡すと「Buzz」を返す', () => {
-    test("5を渡すと文字列'Buzz'を返す", () => {
+    test("5を渡すと文字列「Buzz」を返す", () => {
       expect(fizzBuzz(5)).toBe('Buzz');
     });
   });
